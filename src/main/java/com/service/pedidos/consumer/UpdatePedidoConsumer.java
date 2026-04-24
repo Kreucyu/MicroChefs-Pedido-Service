542b6dbc791c1f6b3a1a7d5bb49cf580c1a5319d
Kreucyu <elciocleiton72@gmail.com>
2026-04-23 23:35:37 -0300

    feat: comunicação completa entre status do pedido com cozinha e pedido service

---
+++ src/main/java/com/service/pedidos/consumer/UpdatePedidoConsumer.java
@@ -0,0 +1,26 @@
+package com.service.pedidos.consumer;
+
+import com.service.pedidos.dto.UpdatePedidoDto;
+import com.service.pedidos.service.PedidoService;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+import tools.jackson.databind.ObjectMapper;
+
+@Component
+public class UpdatePedidoConsumer {
+
+    @Autowired
+    private PedidoService pedidoService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @RabbitListener(queues = { "pedido-queue" })
+    public void receberAtualizacao(@Payload String updateJson) {
+        UpdatePedidoDto update = objectMapper.readValue(updateJson, UpdatePedidoDto.class);
+        pedidoService.atualizarStatusPedido(update);
+    }
+
+}
--- src/main/java/com/service/pedidos/producer/PedidoProducer.java
+++ src/main/java/com/service/pedidos/producer/PedidoProducer.java
@@ -1,7 +1,6 @@
 package com.service.pedidos.producer;
 
 import com.service.pedidos.dto.CozinhaPedidoDto;
-import com.service.pedidos.entities.Pedido;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -13,11 +12,8 @@ public class PedidoProducer {
     @Autowired
     private AmqpTemplate amqpTemplate;
 
-    private final ObjectMapper objectMapper;
-
-    public PedidoProducer(ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
-    }
+    @Autowired
+    private ObjectMapper objectMapper;
 
     public void enviarParaCozinha(CozinhaPedidoDto pedido) {
         amqpTemplate.convertAndSend(
